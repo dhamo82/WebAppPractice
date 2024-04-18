f270c474f90bacc1ad59714643928a50031d49ae
Dhamo <dhamotharan82@gmail.com>
2024-04-18 12:35:14 +0530

    Added webtest project to the solution

---
+++ webtest/WebTest.cs
@@ -0,0 +1,19 @@
+namespace webtest
+{
+    public class WebTest
+    {
+        [Fact]
+        public void DemoTest()
+        {
+            int i = 1;
+            bool result = false;
+            if (i == 1)
+            {
+                result = true;
+                Assert.True(result,"Value should be equal to 1");
+            }
+        
+
+        }
+    }
+}
